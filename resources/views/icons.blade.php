1b34893de6c4ecfbcf75008a4299b7a1740ce8de
Robb Knight <desk@rknight.me>
2024-06-15 13:48:24 +0100

    Fix tint color

--- resources/views/icons.blade.php
+++ resources/views/icons.blade.php
@@ -3,5 +3,5 @@
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
 <link rel="manifest" href="/assets/icons/site.webmanifest">
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
+<meta name="theme-color" content="#2a2525">
 <link rel="icon" type="image/x-icon" href="/assets/icons/favicon.ico">
