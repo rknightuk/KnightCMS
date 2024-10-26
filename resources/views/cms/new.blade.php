074bf91158dd09dea763d6a3a59122a67fade144
Robb Knight <948436+rknightuk@users.noreply.github.com>
2024-10-26 08:51:24 +0100

    Update new.blade.php

--- resources/views/cms/new.blade.php
+++ resources/views/cms/new.blade.php
@@ -226,7 +226,7 @@
 
         <x-label for="date" value="Content" />
 
-        <textarea id="contents" class="border-stone-300 dark:border-stone-700 dark:bg-stone-900 dark:text-stone-300 focus:border-stone-500 dark:focus:border-stone-600 focus:ring-stone-500 dark:focus:ring-stone-600 rounded-md shadow-sm w-full" name="content" rows="20"></textarea>
+        <textarea id="contents" class="border-stone-300 dark:border-stone-700 dark:bg-stone-900 dark:text-stone-300 focus:border-stone-500 dark:focus:border-stone-600 focus:ring-stone-500 dark:focus:ring-stone-600 rounded-md shadow-sm w-full" name="contents" rows="20"></textarea>
 
         <div class="mt-3 flex justify-between">
             <x-button id="preview">
