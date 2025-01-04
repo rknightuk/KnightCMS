a2ffa0e6a5341518a0454f7f615812a556388946
Robb Knight <desk@rknight.me>
2025-01-04 23:25:30 +0000

    Redirect on login

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
